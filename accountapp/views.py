b5a93b5bbe3b31d103f6cac91bf445c5266eab04
alsdn <alsdn319452@gmail.com>
2021-07-12 11:27:10 +0900

    7.12 GIS self commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
     if request.method == "POST":
+
         temp = request.POST.get('input_text')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+
         return render(request, "accountapp/hello_world.html",
-                      context={'text': temp })
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, "accountapp/hello_world.html",
                       context={'text':'GET METHOD!'})
