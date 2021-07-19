53bfbfd2949eb99524b5df811dbaf872ae01f494
alsdn <alsdn319452@gmail.com>
2021-07-19 12:46:33 +0900

    7.19 GIS self commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
+
+
+
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
@@ -14,5 +17,9 @@ urlpatterns = [
 
     path('create/', AccountCreateView.as_view(), name="create"),
 
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete')
 ]
