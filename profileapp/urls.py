6c734a57be45a52279d588576260343f20c9fb26
alsdn <alsdn319452@gmail.com>
2021-07-29 11:20:08 +0900

    7.29 GIS self commit 1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -7,5 +7,5 @@ app_name = 'profileapp'
 urlpatterns = [
     path('create/', ProfileCreateView.as_view(), name='create'),
 
-    path('updatae/<int:pk>', ProfileUpdateView.as_view(), name='update')
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update')
 ]
